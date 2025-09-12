fb232f1369168c439a050904035c4628c272012e
zhuhongdong <zhuhd@qboson.com>
2025-09-12 11:38:38 +0800

    添加qvae单元测试

--- kaiwu/torch_plugin/__init__.py
+++ kaiwu/torch_plugin/__init__.py
@@ -2,5 +2,6 @@
 """玻尔兹曼机"""
 from .restricted_boltzmann_machine import RestrictedBoltzmannMachine
 from .full_boltzmann_machine import BoltzmannMachine
+from .qvae import QVAE
 
 __all__ = ["RestrictedBoltzmannMachine", "BoltzmannMachine"]
