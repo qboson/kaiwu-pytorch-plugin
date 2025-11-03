c716ddd7b55dfe1543848a75120da87d43a09d16
zhuhongdong <zhuhd@boseq.com>
2025-11-03 18:42:17 +0800

    update docs

---
+++ docs/conf.py
@@ -0,0 +1,43 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import os
+import sys
+
+
+sys.path.insert(0, os.path.abspath('..'))
+# sdk所在目录
+sys.path.insert(0, os.path.abspath('../kaiwu/torch_plugin'))
+
+project = 'kaiwu-pytorch-plugin'
+copyright = '2025, qboson'
+author = 'qboson'
+release = '0.0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
